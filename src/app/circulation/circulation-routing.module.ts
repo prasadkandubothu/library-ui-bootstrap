fb39a63e1908f04c394d1aa3f0321e0007f1b3cf
prasadkandubothu <prasad.kandubothu@outlook.com>
2019-09-25 17:43:03 +0530

    circulation module

--- src/app/circulation/circulation-routing.module.ts
+++ src/app/circulation/circulation-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IssuebookComponent } from './issuebook/issuebook.component';
 import { ReturnbookComponent } from './returnbook/returnbook.component';
+import { CirculationComponent } from './circulation/circulation.component';
 
 const routes: Routes = [
+  {
+    path : '',
+    redirectTo : 'circulation',
+    pathMatch : 'full'
+  },
   {
     path : 'issue',
     component : IssuebookComponent
@@ -11,6 +17,10 @@ const routes: Routes = [
   {
     path : 'return',
     component : ReturnbookComponent
+  }, 
+  {
+    path : 'circulation',
+    component : CirculationComponent
   }
 ];
 
--- src/app/circulation/circulation.module.ts
+++ src/app/circulation/circulation.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { IssuebookComponent } from './issuebook/issuebook.component';
+import { ReturnbookComponent } from './returnbook/returnbook.component';
+import { CirculationComponent } from './circulation/circulation.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [IssuebookComponent, ReturnbookComponent, CirculationComponent],
   imports: [
     CommonModule,
     FormsModule,
-
     SharedModule
   ],
-  exports : []
+  exports : [IssuebookComponent, ReturnbookComponent, CirculationComponent]
 })
 export class BooksModule { }
